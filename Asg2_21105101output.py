e8d4669310582d4ad1da558a8f814c7eec13601e
Aditya Singla <96479406+Cooldude1917@users.noreply.github.com>
2022-01-24 14:44:39 +0530

    Add files via upload

---
+++ Asg2_21105101output.py
@@ -0,0 +1,51 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: C:/Users/HP/Asg2_21105101input.py ==================
+
+                       #question_1
+(a) length of string : 35
+(b) reversed string : egaugnal evitisnes esac a si nohtyP
+(c) new string : a case sensitive
+(d) after replacing : Python is object oriented language
+(e) index of a : 10
+(f) after removing white spaces : Pythonisacasesensitivelanguage
+
+                       #question_2
+what is your name : Aditya Singla
+your SID : 21105101
+department name : ECE
+your CGPA : 9.8
+
+Hey, Aditya Singla Here!
+My SID is 21105101 
+I am from ECE department and my CGPA is 9.800000
+
+                      #question_3
+(a) a&b : 8
+(b) a|b : 58
+(c) a^b : 50
+(d) Left shift a with 2 bits : 224
+    Left shift b with 2 bits : 40
+(e) Right shift a with 2 bits : 14
+    Right shift b with 4 bits : 0
+
+                      #question_4
+enter first number : 27
+enter second number : 99.9
+enter third number : 45
+
+the greastest no is : 99.9
+
+                      #question_5
+enter string : Do you know me
+
+no
+
+                      #question_6
+enter length of first side : 25
+enter length of second side : 11
+enter length of third side : 10
+
+no
+
