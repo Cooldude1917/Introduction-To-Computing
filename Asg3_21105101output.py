44a5229c7b4048643b43426ec54a119bfa318a51
Aditya Singla <96479406+Cooldude1917@users.noreply.github.com>
2022-02-13 18:08:19 +0530

    Add files via upload

---
+++ Asg3_21105101output.py
@@ -0,0 +1,83 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: C:\Users\HP\Asg3_21105101input.py ==================
+
+                        Question_1
+enter the string: BharatPe
+bharatpe
+{'b': 1, 'h': 1, 'a': 2, 'r': 1, 't': 1, 'p': 1, 'e': 1}
+
+                          Question_2
+Please enter Day: 26
+Please enter Month: 02
+Please enter Year: 2003
+Next Date is: 27/3/2003
+
+                      Question_3
+Enter the numbers separated by space:11 22 33 44 55 66 77
+
+List containing (n,n^2) is :
+[(11, 121), (22, 484), (33, 1089), (44, 1936), (55, 3025), (66, 4356), (77, 5929)]
+
+                     Question_4
+Enter your grade points: 8
+Your grade is 'B+' and Very Good performance.
+
+                     Question_5
+ABCDEFGHIJK
+ ABCDEFGHI 
+  ABCDEFG  
+   ABCDE   
+    ABC    
+     A     
+
+                      Question 6
+Please Enter SID of Student: 21105101
+Please enter the name of the Student: Aditya Singla
+If you want to enter more details press Y/N: Y
+Please Enter SID of Student: 21107050
+Please enter the name of the Student: Lakshay Garg
+If you want to enter more details press Y/N: Y
+Please Enter SID of Student: 21104080
+Please enter the name of the Student: Akshit Singhal
+If you want to enter more details press Y/N: N
+(a)
+21105101 is Aditya Singla
+21107050 is Lakshay Garg
+21104080 is Akshit Singhal
+
+(b)
+value sorted dictionary is ['Aditya Singla', 'Akshit Singhal', 'Lakshay Garg']
+
+(c)
+Keys sorted dictionary is [21104080, 21105101, 21107050]
+
+(d)
+Please enter SID whose detail you need- 21105101
+Name of the required student is Aditya Singla
+
+
+                      Question_7
+Total elements of Fibonacci sequence that you want(must be greater than 1)- 6
+Fibonnaci sequence with 6 terms
+0
+1
+1
+2
+3
+5
+Sum of total 6 terms of sequence is 12
+END
+
+                   Question_8
+(a)
+set with elements not common in both is {1, 3, 5, 6, 8}
+(b)
+Set of elements that is only present in one set is {17, 3, 6, 8, 9, 13}
+(c)
+Set of elements that is only present in two set is {1, 2, 4, 5}
+(d)
+set of all integers in the range 1 to 10 that are not in Set_1 {6, 7, 8, 9, 10}
+(e)
+set of all integers in the range 1 to 10 that are not in Set_1 and Set_2 and Set_3 {10, 7}
