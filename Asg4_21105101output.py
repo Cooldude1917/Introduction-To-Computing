8661ee85f69a0e32bbb3ca797b17932876399b62
Aditya Singla <96479406+Cooldude1917@users.noreply.github.com>
2022-03-01 22:20:55 +0530

    Add files via upload

---
+++ Asg4_21105101output.py
@@ -0,0 +1,117 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: C:/Users/HP/Asg4_21105101input.py ==================
+
+         #Question_1
+Move Disk 1 from A to C
+Move Disk 2 from A to B
+Move Disk 1 from C to B
+Move Disk 3 from A to C
+Move Disk 1 from B to A
+Move Disk 2 from B to C
+Move Disk 1 from A to C
+
+
+          #Question_2
+Enter number of rows in Pascal's Triangle: 16
+
+#using_recursion_method
+                              1   
+                            1   1   
+                          1   2   1   
+                        1   3   3   1   
+                      1   4   6   4   1   
+                    1   5   10   10   5   1   
+                  1   6   15   20   15   6   1   
+                1   7   21   35   35   21   7   1   
+              1   8   28   56   70   56   28   8   1   
+            1   9   36   84   126   126   84   36   9   1   
+          1   10   45   120   210   252   210   120   45   10   1   
+        1   11   55   165   330   462   462   330   165   55   11   1   
+      1   12   66   220   495   792   924   792   495   220   66   12   1   
+    1   13   78   286   715   1287   1716   1716   1287   715   286   78   13   1   
+  1   14   91   364   1001   2002   3003   3432   3003   2002   1001   364   91   14   1   
+1   15   105   455   1365   3003   5005   6435   6435   5005   3003   1365   455   105   15   1   
+
+#iterative_method
+                              1   
+                            1   1   
+                          1   2   1   
+                        1   3   3   1   
+                      1   4   6   4   1   
+                    1   5   10   10   5   1   
+                  1   6   15   20   15   6   1   
+                1   7   21   35   35   21   7   1   
+              1   8   28   56   70   56   28   8   1   
+            1   9   36   84   126   126   84   36   9   1   
+          1   10   45   120   210   252   210   120   45   10   1   
+        1   11   55   165   330   462   462   330   165   55   11   1   
+      1   12   66   220   495   792   924   792   495   220   66   12   1   
+    1   13   78   286   715   1287   1716   1716   1287   715   286   78   13   1   
+  1   14   91   364   1001   2002   3003   3432   3003   2002   1001   364   91   14   1   
+1   15   105   455   1365   3003   5005   6435   6435   5005   3003   1365   455   105   15   1   
+
+
+           Question_3
+
+Enter Integer:520
+Enter other Integer:27
+
+The quotient when 520 is divided by 27 is 19.
+The remainder when 520 is divided by 27 is 7.
+
+(a)
+
+Function is Not-callable
+
+(b)
+
+All result values are 'non-zero'
+
+(c)
+
+Append (4,5,6) in the values (19,7)
+Values greater than 4 is 19,7,5,6
+
+(d)
+
+Above result in set form is shown below:
+{19, 5, 6, 7}
+
+(e)
+
+The above set converted to immutable.
+
+(f)
+
+Max value from set is 19
+Hash value of 19 is 19
+
+         #Question_4
+Hello, My name is Aditya and my roll no. is 21105101
+Destructor Called
+
+          #Question_5
+Employee Name is Mehak and Salary is 40000 
+Employee Name is Ashok and Salary is 50000 
+Employee Name is Viren and Salary is 60000 
+
+(a)
+Mehak salary Updated to 70000
+Employee Name is Mehak and Salary is 70000 
+
+(b)
+Employee Viren data removed.
+
+           #Question_6
+
+ FRIENDSHIP GAME
+
+Enter Player1 name:George
+Enter Player2 name:Barbie
+
+Enter Word by George:Race
+Enter Word by Barbie:Care
+
+Friendship of George and Barbie is REAL
